6357f9521fdb4ba588261507c09e0a34f3aa9d19
Brian-mula <mulatiwangilabrian@gmail.com>
2022-12-11 03:22:51 +0300

    implemented authentication logic

--- lib/logic/auth_logic.dart
+++ lib/logic/auth_logic.dart
@@ -1,1 +1,51 @@
-class Authentication {}
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class Authentication {
+  FirebaseAuth auth = FirebaseAuth.instance;
+  // !register
+  Future<void> register(
+      BuildContext context, String email, String password) async {
+    try {
+      await auth.createUserWithEmailAndPassword(
+          email: email, password: password);
+    } on FirebaseAuthException catch (e) {
+      await showDialog(
+          context: context,
+          builder: (context) => AlertDialog(
+                title: const Text("Error"),
+                content: Text(e.toString()),
+                actions: [
+                  TextButton(
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      },
+                      child: const Text("Ok"))
+                ],
+              ));
+    }
+  }
+
+  // ! login
+  Future<void> login(
+      String email, String password, BuildContext context) async {
+    try {
+      await auth.signInWithEmailAndPassword(email: email, password: password);
+    } on FirebaseAuthException catch (e) {
+      await showDialog(
+          context: context,
+          builder: (context) => AlertDialog(
+                title: const Text("Error"),
+                content: Text(e.toString()),
+                actions: [
+                  TextButton(onPressed: () {}, child: const Text("Ok"))
+                ],
+              ));
+    }
+  }
+
+  // ! forgot password
+
+  // ! auth state changes
+  Stream<User?> get authStateChange => auth.authStateChanges();
+}
---
+++ lib/views/auth_checker.dart
@@ -0,0 +1,23 @@
+import 'package:emailpos/providers/auth_provider.dart';
+import 'package:emailpos/views/home_view.dart';
+import 'package:emailpos/views/login_view.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class AuthCker extends ConsumerWidget {
+  const AuthCker({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final authState = ref.watch(authStateProvider);
+    return authState.when(
+        data: (data) {
+          if (data != null) return const HomeView();
+          return const LoginView();
+        },
+        error: (error, stacktrace) => Text(error.toString()),
+        loading: () => const Center(
+              child: CircularProgressIndicator(),
+            ));
+  }
+}
