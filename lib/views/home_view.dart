4e94c31852cc181fd5e755f21222640c0c746f74
Brian-mula <mulatiwangilabrian@gmail.com>
2022-12-12 09:24:51 +0300

    completed product display

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -1,3 +1,4 @@
+import 'package:emailpos/providers/products_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -12,6 +13,7 @@ class _HomeViewState extends ConsumerState<HomeView> {
   @override
   Widget build(BuildContext context) {
     ThemeData theme = Theme.of(context);
+    final product = ref.watch(products);
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
@@ -39,47 +41,55 @@ class _HomeViewState extends ConsumerState<HomeView> {
               ],
             ),
             Container(
-              margin: const EdgeInsets.only(top: 25),
-              height: 710,
-              child: ListView.builder(
-                  itemCount: 10,
-                  itemBuilder: (context, index) {
-                    return Container(
-                      margin: const EdgeInsets.only(bottom: 10, top: 0),
-                      child: GestureDetector(
-                        onTap: () {
-                          Navigator.pushNamed(context, "/product-details");
-                        },
-                        child: ListTile(
-                          leading: Container(
-                            height: 150,
-                            width: 100,
-                            decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(6),
-                                image: const DecorationImage(
-                                    image: NetworkImage(
-                                        "https://cdn.pixabay.com/photo/2017/08/06/16/30/computer-2593921__340.jpg"),
-                                    fit: BoxFit.cover)),
-                          ),
-                          title: Text(
-                            "HP Monitor",
-                            style: theme.textTheme.bodyLarge,
-                          ),
-                          subtitle: Text(
-                            "Ksh.30000",
-                            style: theme.textTheme.bodyLarge!
-                                .copyWith(color: Colors.orange.shade600),
-                          ),
-                          trailing: Text(
-                            "30",
-                            style: theme.textTheme.bodyLarge!
-                                .copyWith(color: Colors.orange.shade600),
-                          ),
-                        ),
-                      ),
-                    );
-                  }),
-            )
+                margin: const EdgeInsets.only(top: 25),
+                height: 710,
+                child: product.when(
+                    data: (data) {
+                      return ListView.builder(
+                          itemCount: data.length,
+                          itemBuilder: (context, index) {
+                            return Container(
+                              margin: const EdgeInsets.only(bottom: 10, top: 0),
+                              child: GestureDetector(
+                                onTap: () {
+                                  Navigator.pushNamed(
+                                      context, "/product-details",
+                                      arguments: data[index]);
+                                },
+                                child: ListTile(
+                                  leading: Container(
+                                    height: 150,
+                                    width: 100,
+                                    decoration: BoxDecoration(
+                                        borderRadius: BorderRadius.circular(6),
+                                        image: const DecorationImage(
+                                            image: NetworkImage(
+                                                "https://cdn.pixabay.com/photo/2017/08/06/16/30/computer-2593921__340.jpg"),
+                                            fit: BoxFit.cover)),
+                                  ),
+                                  title: Text(
+                                    data[index].title,
+                                    style: theme.textTheme.bodyLarge,
+                                  ),
+                                  subtitle: Text(
+                                    "Ksh.${data[index].price}",
+                                    style: theme.textTheme.bodyLarge!.copyWith(
+                                        color: Colors.orange.shade600),
+                                  ),
+                                  trailing: Text(
+                                    "30",
+                                    style: theme.textTheme.bodyLarge!.copyWith(
+                                        color: Colors.orange.shade600),
+                                  ),
+                                ),
+                              ),
+                            );
+                          });
+                    },
+                    error: (error, stacktrace) => Text(error.toString()),
+                    loading: () => const Center(
+                          child: CircularProgressIndicator(),
+                        )))
           ],
         ),
       ),
--- lib/views/product_details.dart
+++ lib/views/product_details.dart
@@ -1,3 +1,4 @@
+import 'package:emailpos/models/product_models.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -11,8 +12,13 @@ class ProductDetails extends ConsumerStatefulWidget {
 class _ProductDetailsState extends ConsumerState<ProductDetails> {
   @override
   Widget build(BuildContext context) {
+    final product = ModalRoute.of(context)!.settings.arguments as ProductModel;
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        centerTitle: true,
+        elevation: 0.0,
+        title: Text(product.title),
+      ),
     );
   }
 }
