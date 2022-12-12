6861dfe7ffc9c19327cd9d0fd3ba382760a98719
Brian-mula <mulatiwangilabrian@gmail.com>
2022-12-12 07:36:45 +0300

    completed the homepage

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -11,9 +11,71 @@ class HomeView extends ConsumerStatefulWidget {
 class _HomeViewState extends ConsumerState<HomeView> {
   @override
   Widget build(BuildContext context) {
+    ThemeData theme = Theme.of(context);
     return Scaffold(
       backgroundColor: Colors.white,
-      appBar: AppBar(),
+      appBar: AppBar(
+        elevation: 0.0,
+        backgroundColor: Colors.blue.shade600,
+        centerTitle: true,
+        title: const Text("Products"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.only(left: 10, right: 10, top: 10),
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  "Product name",
+                  style: theme.textTheme.bodyLarge,
+                ),
+                Text(
+                  "In Stock",
+                  style: theme.textTheme.bodyLarge!
+                      .copyWith(color: Colors.orange.shade600),
+                )
+              ],
+            ),
+            Container(
+              margin: const EdgeInsets.only(top: 25),
+              height: 710,
+              child: ListView.builder(
+                  itemCount: 10,
+                  itemBuilder: (context, index) {
+                    return Container(
+                      margin: const EdgeInsets.only(bottom: 10, top: 0),
+                      child: GestureDetector(
+                        onTap: () {},
+                        child: ListTile(
+                          leading: Container(
+                            height: 150,
+                            width: 100,
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(6),
+                                image: const DecorationImage(
+                                    image: NetworkImage(
+                                        "https://cdn.pixabay.com/photo/2017/08/06/16/30/computer-2593921__340.jpg"),
+                                    fit: BoxFit.cover)),
+                          ),
+                          title: Text(
+                            "HP Monitor",
+                            style: theme.textTheme.bodyLarge,
+                          ),
+                          subtitle: Text(
+                            "30",
+                            style: theme.textTheme.bodyLarge!
+                                .copyWith(color: Colors.orange.shade600),
+                          ),
+                        ),
+                      ),
+                    );
+                  }),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
