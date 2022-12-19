5c9169c5a6ca50c45804cd9cc370b1c3c185913e
Brian-mula <mulatiwangilabrian@gmail.com>
2022-12-19 11:03:46 +0300

    customized routes file

---
+++ lib/views/categories.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class CategoriesPage extends ConsumerStatefulWidget {
+  const CategoriesPage({super.key});
+
+  @override
+  ConsumerState<ConsumerStatefulWidget> createState() => _CategoriesPageState();
+}
+
+class _CategoriesPageState extends ConsumerState<CategoriesPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0.0,
+      ),
+    );
+  }
+}
