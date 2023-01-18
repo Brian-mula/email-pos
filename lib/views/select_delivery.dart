ab9019ed325190e3b34909bb176607f70bc65094
Brian-mula <mulatiwangilabrian@gmail.com>
2023-01-18 17:33:09 +0300

    added shipping options

--- lib/views/select_delivery.dart
+++ lib/views/select_delivery.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:roundcheckbox/roundcheckbox.dart';
 
 class SelectDelivery extends ConsumerStatefulWidget {
   const SelectDelivery({super.key});
@@ -9,8 +10,10 @@ class SelectDelivery extends ConsumerStatefulWidget {
 }
 
 class _SelectDeliveryState extends ConsumerState<SelectDelivery> {
+  bool isSelected = true;
   @override
   Widget build(BuildContext context) {
+    ThemeData theme = Theme.of(context);
     return Scaffold(
       body: Stack(
         children: [
@@ -24,6 +27,113 @@ class _SelectDeliveryState extends ConsumerState<SelectDelivery> {
                     image: DecorationImage(
                         image: AssetImage('assets/images/delivery.png'),
                         fit: BoxFit.cover)),
+              )),
+          Positioned(
+              left: 10,
+              right: 10,
+              top: 320,
+              child: Column(
+                children: [
+                  Text(
+                    "Choose a pick up method",
+                    style: theme.textTheme.headline6!
+                        .copyWith(fontSize: 24, color: Colors.orange.shade600),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Row(
+                        children: [
+                          Container(
+                            height: 60,
+                            width: 60,
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(6),
+                                image: const DecorationImage(
+                                    image:
+                                        AssetImage("assets/images/motor.png"),
+                                    fit: BoxFit.cover)),
+                          ),
+                          const SizedBox(
+                            width: 15,
+                          ),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                "Shipping",
+                                style: theme.textTheme.headline6,
+                              ),
+                              const SizedBox(
+                                height: 10,
+                              ),
+                              Text(
+                                "We deliver to your door step",
+                                style: theme.textTheme.bodyLarge,
+                              )
+                            ],
+                          )
+                        ],
+                      ),
+                      RoundCheckBox(
+                          isChecked: isSelected,
+                          onTap: (selected) {
+                            setState(() {
+                              isSelected = !isSelected;
+                            });
+                          })
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 25,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Row(
+                        children: [
+                          Container(
+                            height: 60,
+                            width: 60,
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(6),
+                                image: const DecorationImage(
+                                    image:
+                                        AssetImage("assets/images/walking.png"),
+                                    fit: BoxFit.cover)),
+                          ),
+                          const SizedBox(
+                            width: 15,
+                          ),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                "Self picking",
+                                style: theme.textTheme.headline6,
+                              ),
+                              const SizedBox(
+                                height: 10,
+                              ),
+                              Text(
+                                "You will pick by yourself from our shop",
+                                style: theme.textTheme.bodyLarge,
+                              )
+                            ],
+                          )
+                        ],
+                      ),
+                      RoundCheckBox(
+                          isChecked: false,
+                          onTap: (selected) {
+                            print("tracking selected");
+                          })
+                    ],
+                  )
+                ],
               ))
         ],
       ),
